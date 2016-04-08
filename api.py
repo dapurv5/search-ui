32db4a66f548eedbe9a78fd84b864dd25666cc69
Apurv Verma <dapurv5@gmail.com>
2016-04-08 01:27:42 -0400

    login page, logout via url working smoothly, sweet <3

--- api.py
+++ api.py
@@ -1,3 +1,8 @@
+import time
+
+import flask
+import flask.ext.login as flask_login
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask import render_template
@@ -5,11 +10,98 @@ from flask import send_file, make_response, abort
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
+app.secret_key = 'mojojojo'  # Change this!
+
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+users = {'dapurv5@gmail.com': {'pw': 'secret'}}
+
+
+
+class User(flask_login.UserMixin):
+  pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+  if email not in users:
+    return
+
+  user = User()
+  user.id = email
+  return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+  email = request.form.get('email')
+  if email not in users:
+    return
+
+  user = User()
+  user.id = email
+
+  # DO NOT ever store passwords in plaintext and always compare password
+  # hashes using constant-time comparison!
+  user.is_authenticated = request.form['pw'] == users[email]['pw']
+
+  return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if flask.request.method == 'GET':
+    form = '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'></input>
+                <input type='password' name='pw' id='pw' placeholder='password'></input>
+                <input type='submit' name='submit'></input>
+               </form>
+               '''
+    return make_response(open('static/login.html').read())
+
+  email = flask.request.form['email']
+  print email
+  if flask.request.form['pw'] == users[email]['pw']:
+    user = User()
+    user.id = email
+    flask_login.login_user(user)
+    return flask.redirect(flask.url_for('index'))
+  return flask.redirect(flask.url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+  flask_login.logout_user()
+  return flask.redirect(flask.url_for('login'))
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+  return flask.redirect(flask.url_for('login'))
+
 
 @app.route('/')
-def basic_pages(**kwargs):
+@flask_login.login_required
+def search_page(**kwargs):
   return make_response(open('static/index.html').read())
 
+
+@app.route('/index', methods=['GET', 'POST'])
+@flask_login.login_required
+def index():
+  return make_response(open('static/index.html').read())
+
+
+@app.route('/search/')
+@app.route('/search/<query>')
+def api_article(query=None):
+  time.sleep(1);
+  return 'You are reading ' + str(query)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
 
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,11 +1,24 @@
 'use strict';
 
 
-var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
+var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination', 'angularPromiseButtons'])
+.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
 
 myApp.controller('MainController', function ($scope, $http) {
   $scope.currentPage = 1;
-  $scope.pageSize = 5;
+  $scope.pageSize = 4;
   $scope.docs = [];
   $scope.search = {};
   $scope.search.searchString = "";
@@ -17,23 +30,45 @@ myApp.controller('MainController', function ($scope, $http) {
   //console.log($scope.docs);
   //$scope.searchResults.unshift(JSON.parse(e.data));
 
+
+  //Get search results on button click or Enter key on search box
+  $scope.buttonClick = function(searchString) {
+        console.log('clucked button ' + searchString);
+        $scope.docs = [];
+
+        $scope.yourPromise = $http({
+          method: 'GET',
+          url: 'http://localhost:5000/search/'+searchString
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log("got results for " + searchString);
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+        for (var i = 1; i <= 1000; i++) {
+            var title = "Heading1 "+searchString;
+            var text = "He was visiting the United States when Adolf Hitler came to power in 1933 and, being Jewish, did not"+
+                       "go back to Germany, where he had been a professor at the Berlin Academy of Sciences. He settled in the"+
+                       ".S., becoming an American citizen in 1940.[10] On the eve of World War II, he endorsed a letter to"+
+                       "President Franklin D. Roosevelt alerting him to the potential development of extremely powerful bombs"+
+                       "of a new type and";
+            var doc = {'title': title, 'text': i + text};
+            //$scope.docs.unshift(doc);
+            $scope.docs.push(doc);
+        }
+  };
+
+  //This is a listener for autosuggest kind of feature
   $scope.$watch('search.searchString', function(newVal, oldVal){
-      console.log("Search was changed to:"+newVal);
+      console.log("Search was changed to:"+newVal+" from " + oldVal);
+      //clean docs if necessary
+      //$scope.docs = [];
       $scope.search.watch = newVal;
-      $scope.docs = []
-      for (var i = 1; i <= 50; i++) {
-          var title = "Heading1 "+newVal;
-          var text = "He was visiting the United States when Adolf Hitler came to power in 1933 and, being Jewish, did not"+
-                     "go back to Germany, where he had been a professor at the Berlin Academy of Sciences. He settled in the"+
-                     ".S., becoming an American citizen in 1940.[10] On the eve of World War II, he endorsed a letter to"+
-                     "President Franklin D. Roosevelt alerting him to the potential development of extremely powerful bombs"+
-                     "of a new type and";
-          var doc = {'title': title, 'text': i + text};
-          //$scope.docs.unshift(doc);
-          $scope.docs.push(doc);
-      }
       $scope.currentPage=1;
-      console.log($scope.docs);
+      //console.log($scope.docs);
   });
 });
 
